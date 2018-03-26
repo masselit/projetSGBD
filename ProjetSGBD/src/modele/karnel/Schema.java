3c935758d442a41813178791ba2ff51f3e472dcf
thibautM <34159900+masselit@users.noreply.github.com>
2018-03-26 16:46:06 +0200

    suite structure

--- ProjetSGBD/src/modele/karnel/Schema.java
+++ ProjetSGBD/src/modele/karnel/Schema.java
@@ -3,6 +3,8 @@ package modele.karnel;
 import java.util.ArrayList;
 import java.util.List;
 
+import modele.type.TypeSGBD;
+
 public class Schema implements _Schema{
 	private String nomDeLaRelation;
 	private List<Attribut> attribut;
