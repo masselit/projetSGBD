b2b3540ecd9435c40c4560eec067a5da6f1906d8
VOSSIER <vossier.maxime@gmail.com>
2018-04-05 15:00:22 +0200

    String

--- ProjetSGBD/src/modele/type/StringSGBD.java
+++ ProjetSGBD/src/modele/type/StringSGBD.java
@@ -35,8 +35,4 @@ public class StringSGBD extends TypeSGBD<String> {
 	@Override public String toString() {
 		return "SGBD String";
 	}
-	
-	public int compare(String a,String b) {
-		return a.compareTo(b);
-	}
 }
