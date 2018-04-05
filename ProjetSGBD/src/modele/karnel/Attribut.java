398ba2ca122809864cd208319d738626902cf18b
thibautM <34159900+masselit@users.noreply.github.com>
2018-04-05 14:00:07 +0200

    remise à niveau

---
+++ ProjetSGBD/src/modele/karnel/Attribut.java
@@ -0,0 +1,22 @@
+package modele.karnel;
+
+import modele.type.TypeSGBD;
+
+/**
+ * 
+ * @author Thibaut Masselin
+ *
+ */
+
+public class Attribut<T extends TypeSGBD<?>> {
+	protected String libelle;
+	protected TypeSGBD<T> type;
+	
+	public String toString(){
+		return null;
+	}
+	
+	public Attribut<T> clone(Attribut<T> attr){
+		return null;
+	}
+}
