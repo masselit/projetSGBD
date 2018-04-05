c34b09c66b6bf2f5401c4fc3cc32fae60c8c8f66
thibautM <34159900+masselit@users.noreply.github.com>
2018-04-05 13:50:04 +0200

    add main

---
+++ ProjetSGBD/src/controleur/Main.java
@@ -0,0 +1,10 @@
+package controleur;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Map<ClePrimaire,Tuple> cles ;
+	}
+
+}
--- ProjetSGBD/src/modele/karnel/ClePrimaire.java
+++ ProjetSGBD/src/modele/karnel/ClePrimaire.java
@@ -1,7 +1,16 @@
 package modele.karnel;
 
+
+import modele.type.IntegerSGBD;
 import modele.type.TypeSGBD;
 
 public class ClePrimaire extends Attribut<TypeSGBD> {
+	private TypeSGBD<IntegerSGBD> clePrimaire;
+	
+	public ClePrimaire(IntegerSGBD clePrimaire){
+		this.clePrimaire =(TypeSGBD) clePrimaire;
+	}
+	
+	
 	
 }
