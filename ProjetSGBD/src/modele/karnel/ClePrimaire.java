677bb8d179f32b59191b791c8c6258ba5954a677
thibautM <34159900+masselit@users.noreply.github.com>
2018-04-11 23:33:19 +0200

    add primary key

--- ProjetSGBD/src/modele/karnel/ClePrimaire.java
+++ ProjetSGBD/src/modele/karnel/ClePrimaire.java
@@ -1,12 +1,45 @@
 package modele.karnel;
 
 
-import modele.type.TypeSGBD;
+import java.util.Map;
+import java.util.TreeMap;
 
+/**
+ * Définit une clé primaire
+ *
+ */
 public class ClePrimaire {
+	/**
+	 * Attribut statique stock toutes les clés primaires
+	 */
+	private final static Map<Integer,String> key = new TreeMap<>();
 	
-	public ClePrimaire(String libelle, TypeSGBD<?> type) {
-
+	/**
+	 * Constructeur de la clé primaire
+	 * @param nameRelation
+	 * @param key
+	 * @throws Exception si la clé existe déjà
+	 */
+	public ClePrimaire(String nameRelation,int key) throws Exception{
+		if(uniqueKey(key,nameRelation)){
+			ClePrimaire.key.put(key,nameRelation);
+		}else{
+			throw new IllegalArgumentException("Erreur Clé déjà présente");
+		}
+	}
+	
+	/**
+	 * Garentie que chaque clé prmaire est unique dans ça relation
+	 * @param key
+	 * @param relation
+	 * @return true si la clé primaire n'exite pas et faux si il exite déjà
+	 */
+	private boolean uniqueKey(int key,String relation){
+		if(ClePrimaire.key.containsKey(key) && ClePrimaire.key.get(key).equals(relation)){
+			return false;
+		}else{
+			return true;
+		}
 		
 	}
 	
