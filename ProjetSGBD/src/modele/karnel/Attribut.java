4afbfb3e694d112f92484738c80b29ee7ebbff66
thibautM <34159900+masselit@users.noreply.github.com>
2018-04-12 13:59:01 +0200

    add commentaire karnel

--- ProjetSGBD/src/modele/karnel/Attribut.java
+++ ProjetSGBD/src/modele/karnel/Attribut.java
@@ -3,31 +3,49 @@ package modele.karnel;
 import modele.type.TypeSGBD;
 
 /**
- * 
+ * Permet de definir le nom et le type
  *
  */
 
 public class Attribut {
+	/**
+	 * Attributs
+	 */
 	protected String libelle;
 	protected TypeSGBD<?> type;
 	
+	
+	/**
+	 * Constucteur d'Attribut
+	 * @param libelle sous la forme d'un string
+	 * @param type
+	 */
 	public Attribut(String libelle, TypeSGBD<?> type){
 		this.libelle = libelle;
 		this.type = type;
 	}
 	
+	/**
+	 * Renvoie le type de l'Attribut
+	 * @return un fils de TypeSGBD
+	 */
 	public TypeSGBD<?> getType() {
 		return type;
 	}
-
-	public boolean isClePrimaire(){
-		return false;
-	}
 	
+	/**
+	 * Renvoie le nom de l'Attribut
+	 * @return un String
+	 */
 	public String getName(){
 		return libelle;
 	}
 	
+	/**
+	 * Test l'égalité entre deux Attributs
+	 * @param att est un Attribut
+	 * @return vrai si les attribut ont le même nom et le même type
+	 */
 	public boolean equal(Attribut att) {
 		if ((this.libelle.equals(att.libelle)) && (this.type == att.type)) {
 			return true;
@@ -35,6 +53,10 @@ public class Attribut {
 		return false;
 	}
 	
+	/**
+	 * Renvoie la taille du type
+	 * @return la taille sous fomre de long
+	 */
 	public long size(){
 		return type.taille();
 	}
